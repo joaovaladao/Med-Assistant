70d0e29e0a85fd35c93615795364ff7d2b8532eb
joaovaladao <joaovaladao161274@gmail.com>
2021-03-25 20:32:39 -0300

    #110 Adicionando novas cores para os templates

--- med_app/lib/constants.dart
+++ med_app/lib/constants.dart
@@ -24,3 +24,14 @@ const neutralOrange = Color(0xFFe6961f);
 const darkGray = Color(0xFF595f67);
 const neutralGray = Color(0xFF969ca0);
 const lightGray = Color(0xFFb9b9b9);
+
+class GradientColors {
+  final List<Color> colors;
+  GradientColors(this.colors);
+
+  static List<Color> sky = [Color(0xFF6448FE), Color(0xFF5FC6FF)];
+  static List<Color> sunset = [Color(0xFFFE6197), Color(0xFFFFB463)];
+  static List<Color> sea = [Color(0xFF61A3FE), Color(0xFF63FFD5)];
+  static List<Color> mango = [Color(0xFFFFA738), Color(0xFFFFE130)];
+  static List<Color> fire = [Color(0xFFFF5DCD), Color(0xFFFF8484)];
+}

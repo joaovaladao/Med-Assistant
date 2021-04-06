a901ebfce5aa84a1a428655cea3d5ce2d32be377
joaovaladao <joaovaladao161274@gmail.com>
2021-04-06 15:01:52 -0300

    #118 Função para retornar os valores cadastrados

--- med_app/lib/screens/medicacoes/components/cadastro.dart
+++ med_app/lib/screens/medicacoes/components/cadastro.dart
@@ -5,18 +5,61 @@ import 'package:flutter/material.dart';
 
 
 class Cadastro extends StatelessWidget {
+
+  final _form = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Cadastro"),
+        actions: <Widget>[
+          IconButton(
+           icon: Icon(Icons.save),
+           onPressed: () {  //-------Botão de salvar, quando selecionado printa os dados no terminal
+             _form.currentState.save();
+             Navigator.of(context).pop();
+           }
+          )
+        ],
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Form(
+          key: _form,
+          child: Column(
+            children: <Widget>[
+              TextFormField(  //--------Funções para inserir os dados
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(color: Colors.white),
+                  hintText: "Nome"
+                  ),
+                onSaved: (value){
+                  print(value);
+                },
+              ),
+              TextFormField(
+               style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(color: Colors.white),
+                  hintText: "Quantidade"
+                  ),
+                onSaved: (value){
+                  print(value);
+                },
+              ),
+              TextFormField(
+               style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(color: Colors.white),
+                  hintText: "Dias"
+                  ),
+                onSaved: (value){
+                  print(value);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );

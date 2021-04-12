1ad1db9168f625cd68668d1234a10125b73b4a39
joaovaladao <joaovaladao161274@gmail.com>
2021-04-12 13:17:02 -0300

    #127 Ajustes do template

--- med_app/lib/screens/historico/components/body.dart
+++ med_app/lib/screens/historico/components/body.dart
@@ -20,7 +20,32 @@ class _BodyState extends State<Body> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          TableCalendar(calendarController: calendarController),
+          TableCalendar(
+            calendarStyle: CalendarStyle(
+              markersColor: Colors.white,
+              weekdayStyle: TextStyle(color: Colors.white),
+              todayColor: Colors.orange,
+              selectedColor: Theme.of(context).primaryColor,
+              todayStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                color: Colors.white,
+              ),
+              weekendStyle: TextStyle(color: Colors.white),
+              outsideWeekendStyle: TextStyle(color: Colors.white),
+              renderDaysOfWeek: false,
+            ),
+            headerStyle: HeaderStyle(
+              leftChevronIcon: Icon(Icons.arrow_back_ios, size: 15, color: Colors.white,),
+              rightChevronIcon: Icon(Icons.arrow_forward_ios, size: 15, color: Colors.white,),
+              titleTextStyle: TextStyle(color: Colors.white, fontSize: 25),
+              formatButtonDecoration: BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            calendarController: calendarController
+            ),
         ],
       )
     );

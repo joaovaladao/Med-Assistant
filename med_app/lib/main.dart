a383797c6c43478d954d613de3bd06ab854c4556
joaovaladao <joaovaladao161274@gmail.com>
2021-04-13 12:13:54 -0300

    #130 Ambiente sem conflito com os pacotes de notificações

--- med_app/lib/main.dart
+++ med_app/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:med_app/constants.dart';
 import 'package:med_app/navBar.dart';
-
+  
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

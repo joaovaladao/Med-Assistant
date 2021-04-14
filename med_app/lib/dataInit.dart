e9e44d9c6734045868cab23f06edd6e3bcdac885
joaovaladao <joaovaladao161274@gmail.com>
2021-04-14 17:02:25 -0300

    #117 Configurando o ambiente de trabalho com os pacotes necessários

---
+++ med_app/lib/dataInit.dart
@@ -0,0 +1,44 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+
+final String tableAlarm = 'alarm';
+final String columnId = 'id';
+final String columnTitle = 'title';
+final String columnDateTime = 'alarmDateTime';
+final String columnPending = 'isPending';
+final String columnColorIndex = 'gradientColorIndex';
+
+class DataInit {
+  static Database _database;
+  static DataInit _dataInit;
+
+  DataInit._createInstance();
+  factory DataInit() {
+    if (_dataInit == null) {
+      _dataInit = DataInit._createInstance();
+    }
+    return _dataInit;
+  }
+
+  Future<Database> initializeDatabase() async {
+    var dir = await getDatabasesPath();
+    var path = dir + "alarm.db";
+
+    var database = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {
+        db.execute('''
+          create table $tableAlarm ( 
+          $columnId integer primary key autoincrement, 
+          $columnTitle text not null,
+          $columnDateTime text not null,
+          $columnPending integer,
+          $columnColorIndex integer)
+        ''');
+      },
+    );
+    return database;
+  }
+
+}

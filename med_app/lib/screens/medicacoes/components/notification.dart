ce7013eb0accf2f9186dd436ad89e606bc248429
joaovaladao <joaovaladao161274@gmail.com>
2021-05-05 14:33:09 -0300

    # Criando um arquivo específico para cuidar das notificações

---
+++ med_app/lib/screens/medicacoes/components/notification.dart
@@ -0,0 +1,228 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io' show File, Platform;
+import 'package:http/http.dart' as http;
+import 'package:rxdart/subjects.dart';
+class NotificationPlugin {
+  //
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  final BehaviorSubject<ReceivedNotification>
+      didReceivedLocalNotificationSubject =
+      BehaviorSubject<ReceivedNotification>();
+  var initializationSettings;
+  NotificationPlugin();
+
+NotificationPlugin._() {
+    init();
+  }
+init() async {
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    if (Platform.isIOS) {
+      _requestIOSPermission();
+    }
+    initializePlatformSpecifics();
+  }
+
+initializePlatformSpecifics() {
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('ic_launcher');
+    var initializationSettingsIOS = IOSInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: false,
+      onDidReceiveLocalNotification: (id, title, body, payload) async {
+        ReceivedNotification receivedNotification = ReceivedNotification(
+            id: id, title: title, body: body, payload: payload);
+        didReceivedLocalNotificationSubject.add(receivedNotification);
+      },
+    );
+initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+  }
+_requestIOSPermission() {
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        .requestPermissions(
+          alert: false,
+          badge: true,
+          sound: true,
+        );
+  }
+
+setListenerForLowerVersions(Function onNotificationInLowerVersions) {
+    didReceivedLocalNotificationSubject.listen((receivedNotification) {
+      onNotificationInLowerVersions(receivedNotification);
+    });
+  }
+setOnNotificationClick(Function onNotificationClick) async {
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: (String payload) async {
+      onNotificationClick(payload);
+    });
+  }
+  Future<void> showNotification(int hash, DateTime dateTime, String nome, String description, {String sound}) async {
+    var androidChannelSpecifics = AndroidNotificationDetails(
+      'CHANNEL_ID',
+      'CHANNEL_NAME',
+      "CHANNEL_DESCRIPTION",
+      importance: Importance.Max,
+      priority: Priority.High,
+      sound: RawResourceAndroidNotificationSound('drama_total'),
+      timeoutAfter: 5000,
+      //styleInformation: DefaultStyleInformation(true, true),
+    );
+    var iosChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics =
+        NotificationDetails(androidChannelSpecifics, iosChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      hash,  // Notification ID
+      nome, // Notification Title
+      description, // Notification Body, set as null to remove the body
+      platformChannelSpecifics,
+      payload: hash.toString(), // Notification Payload
+    );
+}
+  /*Future showNotification(int hash, DateTime dateTime, String nome, String description, {String sound}) async{  //Função que aciona Notificação ao dispositivo
+    var androidDetails = new AndroidNotificationDetails("channelId", "channelName", "channelDescription",
+     sound: RawResourceAndroidNotificationSound('drama_total'),
+     priority: Priority.High,
+     importance: Importance.Max,
+     );
+    var iosDetails = new IOSNotificationDetails(sound: 'android_music.wav');
+    var generalNotification = new NotificationDetails(androidDetails, iosDetails);
+      var time = new Time(dateTime.hour,dateTime.minute,dateTime.second);
+      await flutterLocalNotificationsPlugin.showDailyAtTime(
+        hash,
+        nome,
+        description,
+        time,
+        generalNotification);
+  }*/
+
+Future<void> showDailyAtTime() async {
+    var time = Time(21, 3, 0);
+    var androidChannelSpecifics = AndroidNotificationDetails(
+      'CHANNEL_ID 4',
+      'CHANNEL_NAME 4',
+      "CHANNEL_DESCRIPTION 4",
+      importance: Importance.Max,
+      priority: Priority.High,
+    );
+    var iosChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics =
+        NotificationDetails(androidChannelSpecifics, iosChannelSpecifics);
+    await flutterLocalNotificationsPlugin.showDailyAtTime(
+      0,
+      'Test Title at ${time.hour}:${time.minute}.${time.second}',
+      'Test Body', //null
+      time,
+      platformChannelSpecifics,
+      payload: 'Test Payload',
+    );
+  }
+Future<void> showWeeklyAtDayTime() async {
+    var time = Time(21, 5, 0);
+    var androidChannelSpecifics = AndroidNotificationDetails(
+      'CHANNEL_ID 5',
+      'CHANNEL_NAME 5',
+      "CHANNEL_DESCRIPTION 5",
+      importance: Importance.Max,
+      priority: Priority.High,
+    );
+    var iosChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics =
+        NotificationDetails(androidChannelSpecifics, iosChannelSpecifics);
+    await flutterLocalNotificationsPlugin.showWeeklyAtDayAndTime(
+      0,
+      'Test Title at ${time.hour}:${time.minute}.${time.second}',
+      'Test Body', //null
+      Day.Saturday,
+      time,
+      platformChannelSpecifics,
+      payload: 'Test Payload',
+    );
+  }
+Future<void> repeatNotification() async {
+    var androidChannelSpecifics = AndroidNotificationDetails(
+      'CHANNEL_ID 3',
+      'CHANNEL_NAME 3',
+      "CHANNEL_DESCRIPTION 3",
+      importance: Importance.Max,
+      priority: Priority.High,
+      styleInformation: DefaultStyleInformation(true, true),
+    );
+    var iosChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics =
+        NotificationDetails(androidChannelSpecifics, iosChannelSpecifics);
+    await flutterLocalNotificationsPlugin.periodicallyShow(
+      0,
+      'Repeating Test Title',
+      'Repeating Test Body',
+      RepeatInterval.EveryMinute,
+      platformChannelSpecifics,
+      payload: 'Test Payload',
+    );
+  }
+Future<void> scheduleNotification() async {
+    var scheduleNotificationDateTime = DateTime.now().add(Duration(seconds: 5));
+    var androidChannelSpecifics = AndroidNotificationDetails(
+      'CHANNEL_ID 1',
+      'CHANNEL_NAME 1',
+      "CHANNEL_DESCRIPTION 1",
+      //icon: 'secondary_icon',
+      sound: RawResourceAndroidNotificationSound('drama_total'),
+      //largeIcon: DrawableResourceAndroidBitmap('large_notf_icon'),
+      enableLights: true,
+      color: const Color.fromARGB(255, 255, 0, 0),
+      ledColor: const Color.fromARGB(255, 255, 0, 0),
+      ledOnMs: 1000,
+      ledOffMs: 500,
+      importance: Importance.Max,
+      priority: Priority.High,
+      playSound: true,
+      timeoutAfter: 5000,
+      styleInformation: DefaultStyleInformation(true, true),
+    );
+    var iosChannelSpecifics = IOSNotificationDetails(
+      sound: 'my_sound.aiff',
+    );
+    var platformChannelSpecifics = NotificationDetails(
+      androidChannelSpecifics,
+      iosChannelSpecifics,
+    );
+    await flutterLocalNotificationsPlugin.schedule(
+      0,
+      'Test Title',
+      'Test Body',
+      scheduleNotificationDateTime,
+      platformChannelSpecifics,
+      payload: 'Test Payload',
+    );
+  }
+Future<int> getPendingNotificationCount() async {
+    List<PendingNotificationRequest> p =
+        await flutterLocalNotificationsPlugin.pendingNotificationRequests();
+    return p.length;
+  }
+Future<void> cancelNotification() async {
+    await flutterLocalNotificationsPlugin.cancel(0);
+  }
+Future<void> cancelAllNotification() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
+  }
+}
+NotificationPlugin notificationPlugin = NotificationPlugin._();
+class ReceivedNotification {
+  final int id;
+  final String title;
+  final String body;
+  final String payload;
+ReceivedNotification({
+    @required this.id,
+    @required this.title,
+    @required this.body,
+    @required this.payload,
+  });
+}

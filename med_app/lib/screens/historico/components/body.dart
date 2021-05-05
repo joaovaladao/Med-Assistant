a910f07d9c71f51d53567d75aba4cd269ddf73b8
gabrielavelino <blackzinh8@gmail.com>
2021-05-05 17:40:32 -0300

    #148 Consertando erro nas branchs

--- med_app/lib/screens/historico/components/body.dart
+++ med_app/lib/screens/historico/components/body.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:med_app/constants.dart';
+//import 'package:med_app/constants.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -8,120 +10,145 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  CalendarController calendarController;
+  CalendarController _controller;
+  Map<DateTime, List<dynamic>> _events;
+  List<dynamic> _selectedEvents;
+  TextEditingController _eventController;
+  SharedPreferences prefs;
 
+  @override
   void initState() {
     super.initState();
-    calendarController = CalendarController();
+    _controller = CalendarController();
+    _eventController = TextEditingController();
+    _events = {};
+    _selectedEvents = [];
+    initPrefs();
+  }
+
+  initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _events = Map<DateTime, List<dynamic>>.from(
+          decodeMap(json.decode(prefs.getString("events") ?? "{}")));
+    });
+  }
+
+  Map<String, dynamic> encodeMap(Map<DateTime, dynamic> map) {
+    Map<String, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[key.toString()] = map[key];
+    });
+    return newMap;
+  }
+
+  Map<DateTime, dynamic> decodeMap(Map<String, dynamic> map) {
+    Map<DateTime, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[DateTime.parse(key)] = map[key];
+    });
+    return newMap;
   }
 
   @override
   Widget build(BuildContext context) {
-//----------------------------------------------Arquitetura
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        SizedBox(
-          height: 20,
-        ),
-
-        Text(
-          "Calendário",
-          style: TextStyle(
-              fontFamily: 'OpenSans',
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              fontSize: 28),
-        ),
-
-        Container(
-          margin: const EdgeInsets.fromLTRB(10, 30, 10, 10),
-          padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 15),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(18)),
-            gradient: LinearGradient(colors: GradientColors.serrinha),
-          ),
-          //----------------------------------------------Função que retorna calendário
-          child: TableCalendar(
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TableCalendar(
+              events: _events,
+              initialCalendarFormat: CalendarFormat.month,
               calendarStyle: CalendarStyle(
-                //Marcador de evento em baixo de cada dia
-                markersColor: Colors.white,
-                weekdayStyle: TextStyle(fontSize: 20, color: Colors.white),
-                todayColor: Colors.orange,
-                selectedColor: Colors.blue,
-                todayStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                  color: Colors.white,
-                ),
-                weekendStyle: TextStyle(color: Colors.red),
-                outsideWeekendStyle: TextStyle(color: Colors.red),
-                outsideDaysVisible: true,
-                outsideStyle: TextStyle(color: Colors.red),
-                renderDaysOfWeek: true,
-              ),
+                  canEventMarkersOverflow: true,
+                  todayColor: Colors.orange,
+                  selectedColor: Theme.of(context).primaryColor,
+                  todayStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      color: Colors.white)),
               headerStyle: HeaderStyle(
-                leftChevronIcon: Icon(
-                  Icons.arrow_back_ios,
-                  size: 15,
-                  color: Colors.white,
-                ),
-                rightChevronIcon: Icon(
-                  Icons.arrow_forward_ios,
-                  size: 15,
-                  color: Colors.white,
-                ),
-                titleTextStyle: TextStyle(
-                    fontFamily: 'OpenSans',
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 25),
+                centerHeaderTitle: true,
                 formatButtonDecoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
+                formatButtonTextStyle: TextStyle(color: Colors.white),
+                formatButtonShowsNext: false,
               ),
               startingDayOfWeek: StartingDayOfWeek.monday,
-              daysOfWeekStyle: DaysOfWeekStyle(),
-              calendarController: calendarController),
-        ),
-//----------------Faz os botões em baixo do calendario----------------------------------------
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(18)),
-                gradient: LinearGradient(colors: GradientColors.serrinha),
+              /*onDaySelected: (date, events) {
+                setState(() {
+                  _selectedEvents = events;
+                });
+              },*/
+              builders: CalendarBuilders(
+                selectedDayBuilder: (context, date, events) => Container(
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(color: Colors.white),
+                    )),
+                todayDayBuilder: (context, date, events) => Container(
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(color: Colors.white),
+                    )),
               ),
-              child: Text(
-                "Bem =)",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 25),
-              ),
+              calendarController: _controller,
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(18)),
-                gradient: LinearGradient(colors: GradientColors.orange),
-              ),
-              child: Text(
-                "Mal =(",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 25),
-              ),
-            ),
+            ..._selectedEvents.map((event) => ListTile(
+                  title: Text(event),
+                )),
           ],
         ),
-      ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: _showAddDialog,
+      ),
     );
+  }
+
+  _showAddDialog() async {
+    await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              content: TextField(
+                controller: _eventController,
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text("Save"),
+                  onPressed: () {
+                    if (_eventController.text.isEmpty) return;
+                    if (_events[_controller.selectedDay] != null) {
+                      _events[_controller.selectedDay]
+                          .add(_eventController.text);
+                    } else {
+                      _events[_controller.selectedDay] = [
+                        _eventController.text
+                      ];
+                    }
+                    prefs.setString("events", json.encode(encodeMap(_events)));
+                    _eventController.clear();
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            ));
+    setState(() {
+      _selectedEvents = _events[_controller.selectedDay];
+    });
   }
 }
